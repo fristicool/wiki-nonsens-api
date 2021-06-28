a034ea6337769f2aea1ebe7c075b4ae968837785
fristicool <74139390+fristicool@users.noreply.github.com>
2021-06-28 13:02:47 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 // mongodb+srv://fristicool:Donkey@wiki.2um1y.mongodb.net/wiki
 // mongodb://localhost:27017/wiki
-mongoose.connect(process.env.ATLAS, (err) => {
-    if (!err)
-        console.log('Connection succeeded.')
-    else
-        console.log('err in connection: ' + JSON.stringify(err, undefined, 2));
-});
+
+mongoose.connect(process.env.ATLAS, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+// mongoose.connect(process.env.ATLAS, (err) => {
+//     if (!err)
+//         console.log('Connection succeeded.')
+//     else
+//         console.log('err in connection: ' + JSON.stringify(err, undefined, 2));
+// });
 
 mongoose.exports = mongoose; 
